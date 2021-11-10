75ff9edbc8824e1aad78bd32966fd81794e9a6c2
Petronerd1 <bedirhanttepe@gmail.com>
2021-11-10 17:10:40 +0300

    StockUpdate1.2
    StockUpdate1.2

---
+++ Stock/Data/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Data.Models
+{
+    public partial class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int ProductStock { get; set; }
+        public bool ProductStatus { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ Stock/Stock/Controllers/ChartController.cs
@@ -0,0 +1,49 @@
+﻿using Data.DataDb;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Stock.Controllers
+{
+    public class ChartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IActionResult Visual()
+        //{
+        //    return Json(ProList());
+        //}
+        //public List<Product> ProList()
+        //{
+        //    List<Product> pr = new List<Product>();
+
+        //}
+        public IActionResult Index2()
+        {
+            return View();
+
+        }
+        public IActionResult Visual1()
+        {
+            return Json(ProductList());
+        }
+        public List<Chart> ProductList()
+        {
+            List<Chart> ct = new List<Chart>();
+            using (var c = new StockTrackingDbContext())
+            {
+                ct = c.Products.Select(x => new Chart
+                {
+                    Name = x.ProductName,
+                    Stock = x.ProductStock
+                }).ToList();
+            }
+            return ct;
+        }
+    }
+}
